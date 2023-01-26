d17b22cbb49415a003b0e1eb4c1160b235d6a394
Dm7add9 <juha.haipus@gmail.com>
2023-01-26 23:46:00 +0200

    Remove file

--- woltbackend/backend/modules/delivery_fee.py
+++
@@ -1,9 +0,0 @@
-from flask import jsonify
-
-def calculate_fee(content):
-    cart_value = content['cart_value']
-    delivery_distance = content['delivery_distance']
-    number_of_items = content['number_of_items']
-    time = content['time']
-
-    return jsonify({'result': 'success!'})
